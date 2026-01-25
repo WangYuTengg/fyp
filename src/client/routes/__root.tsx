b914e0a5348ace9a05be5f0933a935373bc72c2c
WangYuTengg <wyuteng123@gmail.com>
2026-01-25 20:11:32 +0800

    feat(app): add vercel

--- src/client/routes/__root.tsx
+++ src/client/routes/__root.tsx
@@ -13,17 +13,6 @@ function RootComponent() {
         
         {/* Main content */}
         <div className={user ? "md:pl-64 flex flex-col flex-1" : "flex flex-col flex-1"}>
-          {/* Top bar */}
-          <div className="sticky top-0 z-10 bg-white shadow-sm border-b border-gray-200">
-            <div className="flex h-16 items-center gap-x-4 px-4 sm:gap-x-6 sm:px-6 lg:px-8">
-              <div className="flex flex-1 items-center justify-between">
-                <h1 className="text-lg font-semibold text-gray-900">
-                  UML Assessment Platform
-                </h1>
-              </div>
-            </div>
-          </div>
-
           {/* Page content */}
           <main className="flex-1">
             <div className="py-6">
